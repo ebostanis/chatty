09aa69a63c05a3babb3735196000066cd612b05a
Evripidis Bostanis <evbostanis@gmail.com>
2025-09-22 18:47:04 +0300

    Implement dockerfile and refactor user deletion logic

--- chatty-backend/chatty/src/main/java/org/chattyproject/controllers/UserController.java
+++ chatty-backend/chatty/src/main/java/org/chattyproject/controllers/UserController.java
@@ -5,7 +5,6 @@ import org.chattyproject.dtos.UserResponse;
 import org.chattyproject.models.User;
 import org.chattyproject.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
